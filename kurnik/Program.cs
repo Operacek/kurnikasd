92b07539f1a7819f55535c32c0d2675966807936
Operacek <michalpod99@gmail.com>
2022-05-04 00:54:34 +0200

    Pridani 2 radku do program.cs

--- kurnik/Program.cs
+++ kurnik/Program.cs
@@ -12,6 +12,8 @@ namespace kurnik
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+
         }
     }
 }
